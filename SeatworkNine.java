74f83b93c0de4eff347c4ba264d30d411f61c119
LouieXD100709 <louigiebangero@gmail.com>
2025-09-08 09:51:51 +0000

    Seatwork 7

--- SeatworkNine.java
+++ SeatworkNine.java
@@ -1,11 +1,23 @@
-public class SeatworkNine {
-            public static void main(String args[]) {
-                    int[] numbers = {10, 20, 30, 40, 50};
-                            int index = 0;
-                                    while (index < 5) {
-                                                System.out.print("value of item : " + numbers[index]);
-                                                            index++;
-                                                                    }
-                                                                            System.out.print("\n");
-                                                                                }
-                                                                                }
+public class SeatworkSeven {
+    public static void main(String args[]) {
+        char grade = 'C';
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'B':
+            case 'C':
+                System.out.println("Well done");
+                break;
+            case 'D':
+                System.out.println("You passed");
+                break;
+            case 'F':
+                System.out.println("Better try again");
+                break;
+            default:
+                System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}
