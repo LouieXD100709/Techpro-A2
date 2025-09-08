6122cb981ed1e9026c16a6887e826f62aee39d90
LouieXD100709 <louigiebangero@gmail.com>
2025-09-08 07:42:42 +0000

    Seatwork 11 part 2

--- Main.java
+++ Main.java
@@ -1,15 +1,10 @@
 public class Main {
             public static void main(String[] args) {
-                    System.out.println("SEATWORKELEVEN2025");
-                            System.out.println();
+                    System.out.println("SEATWORKELEVENPART2");
 
-                                    int[][] myNumbers = { {1, 4, 2}, {3, 6, 8, 5, 2} };
+                            int[][] myNumbers = { {1, 4, 2}, {3, 6, 8} };
 
-                                            for (int row = 0; row < myNumbers.length; row++) {
-                                                        for (int col = 0; col < myNumbers[row].length; col++) {
-                                                                        System.out.println("myNumbers[" + row + "][" + col + "] = " + myNumbers[row][col]);
-                                                                                    }
-                                                                                            }
-                                                                                                }
-                                                                                                }
+                                    System.out.println(myNumbers[1][2]);
+                                        }
+                                        }
 }
